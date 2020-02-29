51e8f52af8e22208f8bced4394d087ba3bd5cac3
Dan Charousek <DanCharousek@gmail.com>
2020-02-29 11:56:37 +0100

    Maintain single php version per branch

--- fpm/7.4/base/scripts/001-set_fpm_root_dir.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-[ -z "$FPM_ROOT_DIR" ] && FPM_ROOT_DIR="www"
-
-sed -i -e "s/\$FPM_ROOT_DIR/$FPM_ROOT_DIR/g" \
-    /usr/local/etc/php/php-fpm.d/www.conf \
-    /usr/local/etc/php/php.ini
---
+++ fpm/base/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cd /var/www/data
+
+[ -z $CUSTOM_SCRIPTS_PATH ] && CUSTOM_SCRIPTS_PATH="/var/www/custom-scripts"
+
+if [ -d $CUSTOM_SCRIPTS_PATH ]; then
+  echo "Copying custom scripts"
+  cp $CUSTOM_SCRIPTS_PATH/* /var/www/scripts
+fi
+
+for filename in /var/www/scripts/*.sh; do
+    if [ -f $filename ]; then
+        sh $filename
+    fi
+done
+
+exec php-fpm
