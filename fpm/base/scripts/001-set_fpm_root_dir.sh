0341b0a4297c6ec74ead5d34fc48a9e95bbaacb7
Dan Charousek <DanCharousek@gmail.com>
2020-02-29 15:11:49 +0100

    Do not assume absolute path to data folder

--- fpm/base/scripts/001-set_fpm_root_dir.sh
+++ fpm/base/scripts/001-set_fpm_root_dir.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ -z "$FPM_ROOT_DIR" ] && FPM_ROOT_DIR="www"
+[ -z "$FPM_ROOT_DIR" ] && FPM_ROOT_DIR="/var/www/data/public"
 
 sed -i -e "s/\$FPM_ROOT_DIR/$FPM_ROOT_DIR/g" \
     /usr/local/etc/php/php-fpm.d/www.conf \
