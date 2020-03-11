6783f4c70273638e05ae45a6f223495ca1cf9892
Dan Charousek <DanCharousek@gmail.com>
2020-03-11 10:27:39 +0100

    Use default host ip when env var is not provided

--- fpm/dev/scripts/201-setup_xdebug.sh
+++ fpm/dev/scripts/201-setup_xdebug.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
+HOST_IP=$(ip route show default | awk '/default/ {print $3}')
+
 [ -z "$XDEBUG_EXTENSION_PATH" ] && XDEBUG_EXTENSION_PATH="xdebug.so"
 [ -z "$XDEBUG_ENABLE" ] && XDEBUG_ENABLE="1"
+[ -z "$XDEBUG_AUTOSTART" ] && XDEBUG_AUTOSTART="0"
 [ -z "$XDEBUG_PORT" ] && XDEBUG_PORT="9999"
 [ -z "$XDEBUG_IDEKEY" ] && XDEBUG_IDEKEY="docker"
 [ -z "$XDEBUG_HANDLER" ] && XDEBUG_HANDLER="dbgp"
 [ -z "$XDEBUG_CONNECT_BACK" ] && XDEBUG_CONNECT_BACK="0"
-[ -z "$XDEBUG_HOST" ] && XDEBUG_HOST="0"
-
-if [ "$XDEBUG_HOST" == "0" ]; then
-    echo "Xdebug host was not set, disabling"
-    XDEBUG_ENABLE="0"
-fi
+[ -z "$XDEBUG_HOST" ] && XDEBUG_HOST="$HOST_IP"
 
 # Replace placeholders with env variables
 sed -i -e "
 s#\$XDEBUG_EXTENSION_PATH#$XDEBUG_EXTENSION_PATH#g
 s#\$XDEBUG_ENABLE#$XDEBUG_ENABLE#g
+s#\$XDEBUG_AUTOSTART#$XDEBUG_AUTOSTART#g
 s#\$XDEBUG_PORT#$XDEBUG_PORT#g
 s#\$XDEBUG_IDEKEY#$XDEBUG_IDEKEY#g
 s#\$XDEBUG_HANDLER#$XDEBUG_HANDLER#g
