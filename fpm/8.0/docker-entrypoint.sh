67d7f983991dbdebbd23be37a2c14b4c5ac8fc08
Dan Charousek <DanCharousek@gmail.com>
2021-02-04 21:27:26 +0100

    Update entrypoint script

--- fpm/8.0/docker-entrypoint.sh
+++ fpm/8.0/docker-entrypoint.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 
-mkdir -p /var/www/data
-cd /project/
+[[ -z "${APP_DIR}" ]] && APP_DIR="/var/www"
+[[ -z "${INIT_DIR}" ]] && INIT_DIR="/opt/litea/init.d"
 
-[[ -z $CUSTOM_SCRIPTS_PATH ]] && CUSTOM_SCRIPTS_PATH="/var/www/user-init.d"
+mkdir -p "${APP_DIR}"
+cd "${APP_DIR}"
 
-if [[ -d $CUSTOM_SCRIPTS_PATH ]]; then
-  cp -r "$CUSTOM_SCRIPTS_PATH/." /var/www/init.d
+[[ -z "${CUSTOM_SCRIPTS_PATH}" ]] && CUSTOM_SCRIPTS_PATH="/opt/litea/user-init.d"
+
+if [[ -d "${CUSTOM_SCRIPTS_PATH}" ]]; then
+  cp -r "${CUSTOM_SCRIPTS_PATH}/." "${INIT_DIR}"
 fi
 
-for filename in /var/www/init.d/*.sh; do
-    if [[ -f $filename ]]; then
-        sh $filename
+for filename in "${INIT_DIR}/*.sh"; do
+    if [[ -f "${filename}" ]]; then
+        sh "${filename}"
+
+        if [[ "${?}" != 0 ]]; then
+            echo "ERROR: init script ${filename} exitted with non-zero code"
+            exit 1
+        fi
     fi
 done
 
-exec "$@"
+exec "${@}"
