252465ab68a08be63ad0cb0e115885b3e79d55d7
Dan Charousek <DanCharousek@gmail.com>
2021-02-04 21:30:42 +0100

    Add env variable to disable init scripts

--- fpm/8.0/docker-entrypoint.sh
+++ fpm/8.0/docker-entrypoint.sh
@@ -2,25 +2,27 @@
 
 [[ -z "${APP_DIR}" ]] && APP_DIR="/var/www"
 [[ -z "${INIT_DIR}" ]] && INIT_DIR="/opt/litea/init.d"
+[[ -z "${RUN_INIT_SCRIPTS}" ]] && RUN_INIT_SCRIPTS="1"
+[[ -z "${CUSTOM_SCRIPTS_PATH}" ]] && CUSTOM_SCRIPTS_PATH="/opt/litea/user-init.d"
 
 mkdir -p "${APP_DIR}"
 cd "${APP_DIR}"
 
-[[ -z "${CUSTOM_SCRIPTS_PATH}" ]] && CUSTOM_SCRIPTS_PATH="/opt/litea/user-init.d"
-
-if [[ -d "${CUSTOM_SCRIPTS_PATH}" ]]; then
-  cp -r "${CUSTOM_SCRIPTS_PATH}/." "${INIT_DIR}"
-fi
-
-for filename in "${INIT_DIR}/*.sh"; do
-    if [[ -f "${filename}" ]]; then
-        sh "${filename}"
-
-        if [[ "${?}" != 0 ]]; then
-            echo "ERROR: init script ${filename} exitted with non-zero code"
-            exit 1
-        fi
+if [[ ! -z "${RUN_INIT_SCRIPTS}" && "${RUN_INIT_SCRIPTS}" == "1" ]]; then
+    if [[ -d "${CUSTOM_SCRIPTS_PATH}" ]]; then
+        cp -r "${CUSTOM_SCRIPTS_PATH}/." "${INIT_DIR}"
     fi
-done
+
+    for filename in "${INIT_DIR}/*.sh"; do
+        if [[ -f "${filename}" ]]; then
+            sh "${filename}"
+
+            if [[ "${?}" != 0 ]]; then
+                echo "ERROR: init script ${filename} exitted with non-zero code"
+                exit 1
+            fi
+        fi
+    done
+fi
 
 exec "${@}"
