999e8de5b1ac39afb39691723e89c402993eb01b
Dan Charousek <DanCharousek@gmail.com>
2021-02-28 20:31:44 +0100

    Change the vendor dir ownership if the uid is specified

--- fpm/8.0/conf/composer/init.d/101-composer-run_composer_install.sh
+++ fpm/8.0/conf/composer/init.d/101-composer-run_composer_install.sh
@@ -6,3 +6,10 @@ if [[ ! -z "${COMPOSER_AUTO_INSTALL}" ]] && [[ "${COMPOSER_AUTO_INSTALL}" = 1 ]]
         --prefer-dist \
         --no-suggest
 fi
+
+if [[ ! -z "${FPM_USER_ID}" ]] ; then
+    echo "FPM_USER_ID specified: ${FPM_USER_ID}"
+    echo "Changing ownership of the vendor directory"
+
+    chown -R "${FPM_USER_ID}:${FPM_USER_ID}" vendor
+fi
