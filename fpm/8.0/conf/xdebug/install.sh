6863797c6f067c493fecc9fb2628abf52aa3fba2
Chrudos Vorlicek <chrudos.vorlicek@gmail.com>
2021-05-12 08:57:02 +0200

    Change configuration of Xdebug from v2 to v3 (#5)

--- fpm/8.0/conf/xdebug/install.sh
+++ fpm/8.0/conf/xdebug/install.sh
@@ -8,6 +8,6 @@ cp -R "${current_dir}/init.d/." "${container_init_directory}"
 
 echo "Installing xdebug extension"
 apk add --no-cache --virtual .build-deps $PHPIZE_DEPS \
-    && pecl install xdebug-3.0.2 \
+    && pecl install xdebug-3.0.4 \
     && docker-php-ext-enable xdebug \
     && apk del .build-deps
